771cc697504a7e9aba3483077c6e95275f35d4ef
shadw77 <Shady2018170763@cis.asu.edu.eg>
2025-04-21 23:24:57 +0200

    Store skill

--- resources/js/Pages/Skills/Create.vue
+++ resources/js/Pages/Skills/Create.vue
@@ -9,13 +9,60 @@
         </template>
 
         <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                
+            <div class="mx-auto max-w-md sm:px-6 lg:px-8 bg-white">
+                <form class="p-4" @submit.prevent="submit">
+                    <div>
+                        <InputLabel for="name" value="Name" />
+                        <TextInput
+                            id="name"
+                            type="text"
+                            class="mt-1 block w-full"
+                            v-model="form.name"
+                            autofocus
+                            autocomplete="username"
+                        />
+                        <InputError class="mt-2" :message="form.errors.name" />
+                    </div>
+
+                    <div class="mt-2">
+                        <InputLabel for="image" value="Image" />
+                        <TextInput
+                            id="image"
+                            type="file"
+                            class="mt-1 block w-full"
+                            @input="form.image = $event.target.files[0]"
+                        />
+                        <InputError class="mt-2" :message="form.errors.image" />
+                    </div>
+
+                    <div class="mt-4 flex items-center justify-end">
+                        <PrimaryButton
+                            class="ms-4"
+                            :class="{ 'opacity-25': form.processing }"
+                            :disabled="form.processing"
+                        >
+                            Store
+                        </PrimaryButton>
+                    </div>
+                </form>
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head, Link } from "@inertiajs/vue3";
+import { Head, useForm } from "@inertiajs/vue3";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+
+const form = useForm({
+    name: "",
+    image: null,
+});
+
+const submit = () => {
+    form.post(route("skills.store"));
+};
 </script>
